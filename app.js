a0a1b194b407520a657a12f21d35d057766d22fc
Juan Cruz <jc23729@gmail.com>
2021-01-25 00:46:25 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+while (!player1){
+    var player1 = prompt('Player One: Enter your name. You will be red.');
+};
+var player1Color = 'red';
+
+while (!player2){
+    var player2 = prompt('Player Two: Enter your name. You will be yellow.');
+};
+var player2Color = 'yellow';
+
