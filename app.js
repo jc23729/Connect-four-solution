9c73b41e06b7e7db05f3a343de31687a243fbe3a
Juan Cruz <jc23729@gmail.com>
2021-01-25 00:47:29 +0000

    Selectors

--- app.js
+++ app.js
@@ -8,3 +8,26 @@ while (!player2){
 };
 var player2Color = 'yellow';
 
+// Selectors
+
+
+var tableRow = document.getElementsByTagName('tr');
+var tableData = document.getElementsByTagName('td');
+var playerTurn = document.querySelector('.player-turn');
+const slots = document.querySelectorAll('.slot');
+const resetBtn = document.querySelector('.reset');
+
+
+var currentPlayer = 1;
+let winner;
+playerTurn.textContent = `${player1}'s turn!`
+
+// Log cell coordinates when clicked
+
+for (i = 0; i < tableData.length; i ++){
+    tableData[i].addEventListener('click', (e) =>{
+        console.log(`${e.target.parentElement.rowIndex},${e.target.cellIndex}`)
+    });
+};
+
+
